629886e76d9ba30bc85d1475ba0e151231823ec7
Graham Barber <grahammbarber@gmail.com>
2015-10-29 21:54:41 -0700

    Fixed indentation globally

--- src/js/game.js
+++ src/js/game.js
@@ -1,56 +1,56 @@
 (function() {
-  'use strict';
+	'use strict';
 
-  function Game() {}
-  
-  var player;
-  var floor;
-  var wall;
-  var floorLine;
-  var playerY;
-  var cursors;
+	function Game() {}
 
-  Game.prototype = {
-    create: function () {
-      this.input.onDown.add(this.onInputDown, this);
-      floorLine = this.game.height - 128;
-      playerY = floorLine + 32;
-      
-      player = this.game.add.sprite(0, playerY, 'player');
-  
-      wall = this.game.add.sprite(0, floorLine - 128, 'wall');
-      wall.moveDown();
-      
-      floor = this.game.add.sprite(0, floorLine, 'floor');
-      floor.moveDown();
-      
-      this.input.keyboard.addKey(Phaser.Keyboard.UP);
-      cursors = this.game.input.keyboard.createCursorKeys();
-    },
+	var player;
+	var floor;
+	var wall;
+	var floorLine;
+	var playerY;
+	var cursors;
 
-    update: function () { 
-      
-      if (cursors.left.isDown)
-        player.x += -5;
-      else if (cursors.right.isDown)
-        player.x += 5;
-      
-      this.game.input.keyboard.onDownCallback = function( e ){
-        if (e.keyCode == Phaser.Keyboard.UP) 
-          this.game.add.tween(player).to( { x: player.x, y: floorLine - player.height }, 500, "Cubic", true);
-      };
-      
-      this.game.input.keyboard.onUpCallback = function( e ){
-        if(e.keyCode == Phaser.Keyboard.UP)
-          this.game.add.tween(player).to( { x: player.x, y: playerY }, 500, "Cubic", true);
-      };
-    },
+	Game.prototype = {
+		create: function () {
+			this.input.onDown.add(this.onInputDown, this);
+			floorLine = this.game.height - 128;
+			playerY = floorLine + 32;
 
-    onInputDown: function () {
-      this.game.state.start('menu');
-    }
-  };
+			player = this.game.add.sprite(0, playerY, 'player');
 
-  window['s6'] = window['s6'] || {};
-  window['s6'].Game = Game;
+			wall = this.game.add.sprite(0, floorLine - 128, 'wall');
+			wall.moveDown();
+
+			floor = this.game.add.sprite(0, floorLine, 'floor');
+			floor.moveDown();
+
+			this.input.keyboard.addKey(Phaser.Keyboard.UP);
+			cursors = this.game.input.keyboard.createCursorKeys();
+		},
+
+		update: function () {
+
+			if (cursors.left.isDown)
+				player.x += -5;
+			else if (cursors.right.isDown)
+				player.x += 5;
+
+			this.game.input.keyboard.onDownCallback = function( e ){
+				if (e.keyCode == Phaser.Keyboard.UP)
+					this.game.add.tween(player).to( { x: player.x, y: floorLine - player.height }, 500, "Cubic", true);
+			};
+
+			this.game.input.keyboard.onUpCallback = function( e ){
+				if(e.keyCode == Phaser.Keyboard.UP)
+					this.game.add.tween(player).to( { x: player.x, y: playerY }, 500, "Cubic", true);
+			};
+		},
+
+		onInputDown: function () {
+			this.game.state.start('menu');
+		}
+	};
+
+	window['s6'] = window['s6'] || {};
+	window['s6'].Game = Game;
 }());
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,13 @@
 /* global Phaser */
 window.addEventListener('load', function () {
-  'use strict';
+	'use strict';
 
-  var ns = window['s6'];
-  var game = new Phaser.Game(1024, 600, Phaser.AUTO, 's6-game');
-  game.state.add('boot', ns.Boot);
-  game.state.add('preloader', ns.Preloader);
-  game.state.add('menu', ns.Menu);
-  game.state.add('game', ns.Game);
-  /* yo phaser:state new-state-files-put-here */
-  game.state.start('boot');
+	var ns = window['s6'];
+	var game = new Phaser.Game(1024, 600, Phaser.AUTO, 's6-game');
+	game.state.add('boot', ns.Boot);
+	game.state.add('preloader', ns.Preloader);
+	game.state.add('menu', ns.Menu);
+	game.state.add('game', ns.Game);
+	/* yo phaser:state new-state-files-put-here */
+	game.state.start('boot');
 }, false);
