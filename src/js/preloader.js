160ba0defcfa023aa4dff25107adfa2fa62ab90e
Graham Barber <grahammbarber@gmail.com>
2015-10-30 23:07:37 -0700

    Added menu items

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -24,25 +24,28 @@
 			this.game.load.baseURL = '/assets/';
 			this.game.load.crossOrigin = 'anonymous';
 
+			// ----- Menu -----
+			this.game.load.image('ruby', 'kenny/Tiles/greystone_ruby.png');
+			this.game.load.image('bricks', 'kenny/Tiles/brick_red.png');
+
 			// ----- Background -----
 			this.game.load.image('player', 'kenny/Characters/Hedgehog/hedgehog_body.png');
-			this.game.load.image('platform', 'kenny/Tiles/brick_red.png');
 			this.game.load.image('well', 'backgrounds/well.png');
 			this.game.load.image('screen', 'backgrounds/screen.png');
 
 			this.game.load.image('lwall', 'backgrounds/lower wall.png');
 			this.game.load.image('uwall', 'backgrounds/upper wall.png');
 			this.game.load.image('floor', 'backgrounds/floor.png');
-			
+
 			// ----- Smoke -----
 			this.game.load.image('smoke1', 'effects/smoke 1.png');
 			this.game.load.image('smoke2', 'effects/smoke 2.png');
 			this.game.load.image('smoke3', 'effects/smoke 3.png');
-			
+
 			// ----- Face Stuff -----
 			// Popup
 			this.game.load.image('popup', 'popup.png');
-			
+
 			// Eyes
 			this.game.load.image('eyes0', 'face elements/bored eyes.png');
 			this.game.load.image('eyes1', 'face elements/mad eyes.png');
@@ -50,7 +53,7 @@
 			this.game.load.image('eyes3', 'face elements/pleased eyes.png');
 			this.game.load.image('eyes4', 'face elements/sad eyes.png');
 			this.game.load.image('eyes5', 'face elements/suspicious eyes.png');
-			
+
 			// Nose
 			this.game.load.image('nose0', 'face elements/cute nose.png');
 			this.game.load.image('nose1', 'face elements/long nose.png');
@@ -58,7 +61,7 @@
 			this.game.load.image('nose3', 'face elements/pointy nose.png');
 			this.game.load.image('nose4', 'face elements/squat nose.png');
 			this.game.load.image('nose5', 'face elements/strong nose.png');
-			
+
 			// Mouth
 			this.game.load.image('mouth0', 'face elements/bored mouth.png');
 			this.game.load.image('mouth1', 'face elements/mad mouth.png');
@@ -66,16 +69,16 @@
 			this.game.load.image('mouth3', 'face elements/pleased mouth.png');
 			this.game.load.image('mouth4', 'face elements/sad mouth.png');
 			this.game.load.image('mouth5', 'face elements/worried mouth.png');
-			
+
 			// Ears
 			this.game.load.image('ears0', 'face elements/white ears med.png');
-			
+
 			// Front hair
 			this.game.load.image('front0', 'face elements/normal hair fore.png');
-			
+
 			// Back hair
 			this.game.load.image('back0', 'face elements/girl hair back.png');
-			
+
 			// Face
 			this.game.load.image('face0', 'face elements/facebase white.png');
 		},
