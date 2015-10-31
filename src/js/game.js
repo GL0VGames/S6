61cfeea7f59dc7fdce01ccc6339180830c5bc556
dlkulp <damiankulp@gmail.com>
2015-10-30 22:12:30 -0700

    faces not showing but otherwise playable

--- src/js/game.js
+++ src/js/game.js
@@ -8,7 +8,7 @@
 	var playerState = "walking";
 	var playerDir = {left: 1, right: -1};
 	var currPlayerDir = playerDir.left;
-	var hidingPlayerTheatre;
+	var hidingPlayerTheatre = 0;
 	var floor;
 	var theatres;
 	var floorLine;
@@ -16,15 +16,16 @@
 	var walkingPlayerY;
 	var cursors;
 	var faceLoc;
-	var face;
 	var throwKey;
 	
-	var TheatreParts = {WALL: 0, WELL: 1, SCREEN: 2};
+	var TheatreParts = {LWALL: 0, UWALL: 1, WELL: 2, SCREEN: 3};
 	
 	// Global Vars
 	var MOVEMENT_SPEED = 5;
 	var TOTAL_THEATRES = 6;
 	var THEATRE_WIDTH = 474;
+	var MAX_FACE = 1;
+	var MAX_ALL_ELSE = 1;
 	var worldWidth = TOTAL_THEATRES * THEATRE_WIDTH;
 	var currentTheatrePosX = 0;
 
@@ -49,26 +50,31 @@
 		create: function(y, context) {
 			var x = currentTheatrePosX + THEATRE_WIDTH / 2;
 			var ret = {
-				_: [context.game.add.sprite(currentTheatrePosX, y, 'wall'), context.game.add.sprite(x, y, 'well'), context.game.add.sprite(x, 100, 'screen')],
-				em: context.game.add.emitter(currentTheatrePosX + THEATRE_WIDTH / 2, 200, 100)
+				_: [context.game.add.sprite(currentTheatrePosX, y, 'lwall'), context.game.add.sprite(currentTheatrePosX, y, 'uwall'), context.game.add.sprite(x, y, 'well'), context.game.add.sprite(currentTheatrePosX, y, 'screen')],
+				em: context.game.add.emitter(currentTheatrePosX + THEATRE_WIDTH / 2, 200, 100),
+				face: FaceManager.create(currentTheatrePosX + THEATRE_WIDTH / 2, 300, context)
 			}
 			this.setup(ret, context);
 			return ret;
 		},
 		setup: function (obj, context) {
-			// Wall
+			// Lower Wall
 			obj._[0].scale.set(.5);
 			obj._[0].anchor.set(0, 1);
 			
-			// Well
+			// Upper Wall
+			obj._[1].anchor.set(0, 1);
 			obj._[1].scale.set(.5);
-			obj._[1].anchor.set(.5, 1);
-			obj._[1].tint = 0xd0d0d0;
+			
+			// Well
+			obj._[2].scale.set(.5);
+			obj._[2].anchor.set(.5, 1);
+			obj._[2].tint = 0xd0d0d0;
 			context.game.physics.arcade.enable(obj._[1]);
 
 			// Screen
-			obj._[2].anchor.set(.5, 1);
-			obj._[2].scale.set(.5);
+			obj._[3].scale.set(.5);
+			obj._[3].anchor.set(0, 1);
 			
 			// Particles
 			obj.em.makeParticles(['smoke1', 'smoke2', 'smoke3']);
@@ -76,6 +82,24 @@
 			obj.em.maxParticleScale = .3;
 		}
 	}
+	
+	var FaceManager = {
+		create: function(x, y, context) {
+			var baseColor = Math.floor(Math.random() * MAX_FACE);
+			var faceGroup = context.game.add.physicsGroup();
+			faceGroup.create(x, y, 'back' + Math.floor(Math.random() * MAX_ALL_ELSE));
+			faceGroup.create(x, y, 'ears' + baseColor);
+			faceGroup.create(x, y, 'face' + baseColor);
+			faceGroup.create(x, y, 'front' + Math.floor(Math.random() * MAX_ALL_ELSE));
+			faceGroup.create(x, y, 'eyes' + Math.floor(Math.random() * MAX_ALL_ELSE));
+			faceGroup.create(x, y, 'nose' + Math.floor(Math.random() * MAX_ALL_ELSE));
+			faceGroup.create(x, y, 'mouth' + Math.floor(Math.random() * MAX_ALL_ELSE));
+			faceGroup.setAll('visible', false);
+			faceGroup.setAll('z', 99);
+			return faceGroup;
+		}
+	}
+	
 
 	Game.prototype = {
 		create: function () {
@@ -84,7 +108,6 @@
 			floorLine = this.game.height - 128;
 			hidingPlayerY = floorLine - 20;
 			walkingPlayerY = floorLine + 40;
-			faceLoc = {x: this.game.width / 2, y: this.game.height / 2};
 			
 			theatres = TheatreManager.create(floorLine, this);
 			
@@ -98,28 +121,12 @@
 			this.game.physics.arcade.enable(player);
 			this.game.camera.follow(player, Phaser.Camera.FOLLOW_TOPDOWN);
 
-			face = this.game.add.physicsGroup();
-			face.create(faceLoc.x, faceLoc.y, 'hairB1').scale.setTo(0.2);
-			face.create(faceLoc.x, faceLoc.y, 'ears1').scale.setTo(0.2);
-			face.create(faceLoc.x, faceLoc.y, 'face4').scale.setTo(0.2);
-			face.create(faceLoc.x, faceLoc.y, 'hairF1').scale.setTo(0.2);
-			face.create(faceLoc.x, faceLoc.y, 'eyes1').scale.setTo(0.2);
-			face.create(faceLoc.x, faceLoc.y, 'nose1').scale.setTo(0.2);
-			face.create(faceLoc.x, faceLoc.y, 'mouth1').scale.setTo(0.2);
-			face.setAll('body.immovable', true);
-			face.setAll('visible', false);
-
 			this.input.keyboard.addKey(Phaser.Keyboard.UP);
 			cursors = this.game.input.keyboard.createCursorKeys();
 			throwKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
 		},
 
-		update: function () {
-			// if (player.overlap(wall) && player.y === hidingPlayerY)
-			// 	face.setAll('visible', true);
-			// else if (player.y === walkingPlayerY)
-			// 	face.setAll('visible', false);
-			
+		update: function () {			
 			if (playerState === "walking") {
 				var wells = theatres.getParts(TheatreParts.WELL);
 				
@@ -150,10 +157,9 @@
 				player.tint = 0x747474;
 				if (throwKey.isDown && playerState === "hiding") {
 					theatres.theatres[hidingPlayerTheatre].em.start(false, 5000, 20);
-					//this.game.physics.arcade.collide(theatres.theatres[hidingPlayerTheatre].em);
-					playerState = "done";
+					playerState = "smoking";
 				}
-					
+				theatres.theatres[hidingPlayerTheatre].face.setAll('visible', true);
 			}
 			else if (playerState === "moving") {
 				player.tint = 0xffffff;
