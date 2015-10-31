b5a8e5c71e5d9067f3f0297e348b5e8cedb97069
dlkulp <damiankulp@gmail.com>
2015-10-30 20:49:24 -0700

    fixed faulty paths

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -40,40 +40,40 @@
 			
 			// ----- Face Stuff -----
 			// Eyes
-			this.game.load.image('eyes0', 'effects/bored eyes.png');
-			this.game.load.image('eyes1', 'effects/mad eyes.png');
-			this.game.load.image('eyes2', 'effects/normal eyes.png');
-			this.game.load.image('eyes3', 'effects/pleased eyes.png');
-			this.game.load.image('eyes4', 'effects/sad eyes.png');
-			this.game.load.image('eyes5', 'effects/suspicious eyes.png');
+			this.game.load.image('eyes0', 'face elements/bored eyes.png');
+			this.game.load.image('eyes1', 'face elements/mad eyes.png');
+			this.game.load.image('eyes2', 'face elements/normal eyes.png');
+			this.game.load.image('eyes3', 'face elements/pleased eyes.png');
+			this.game.load.image('eyes4', 'face elements/sad eyes.png');
+			this.game.load.image('eyes5', 'face elements/suspicious eyes.png');
 			
 			// Nose
-			this.game.load.image('nose0', 'effects/cute nose.png');
-			this.game.load.image('nose1', 'effects/long nose.png');
-			this.game.load.image('nose2', 'effects/normal nose.png');
-			this.game.load.image('nose3', 'effects/pointy nose.png');
-			this.game.load.image('nose4', 'effects/squat nose.png');
-			this.game.load.image('nose5', 'effects/strong nose.png');
+			this.game.load.image('nose0', 'face elements/cute nose.png');
+			this.game.load.image('nose1', 'face elements/long nose.png');
+			this.game.load.image('nose2', 'face elements/normal nose.png');
+			this.game.load.image('nose3', 'face elements/pointy nose.png');
+			this.game.load.image('nose4', 'face elements/squat nose.png');
+			this.game.load.image('nose5', 'face elements/strong nose.png');
 			
 			// Mouth
-			this.game.load.image('mouth0', 'effects/bored mouth.png');
-			this.game.load.image('mouth1', 'effects/mad mouth.png');
-			this.game.load.image('mouth2', 'effects/normal mouth.png');
-			this.game.load.image('mouth3', 'effects/pleased mouth.png');
-			this.game.load.image('mouth4', 'effects/sad mouth.png');
-			this.game.load.image('mouth5', 'effects/worried mouth.png');
+			this.game.load.image('mouth0', 'face elements/bored mouth.png');
+			this.game.load.image('mouth1', 'face elements/mad mouth.png');
+			this.game.load.image('mouth2', 'face elements/normal mouth.png');
+			this.game.load.image('mouth3', 'face elements/pleased mouth.png');
+			this.game.load.image('mouth4', 'face elements/sad mouth.png');
+			this.game.load.image('mouth5', 'face elements/worried mouth.png');
 			
 			// Ears
-			this.game.load.image('ears0', 'effects/white ears med.png');
+			this.game.load.image('ears0', 'face elements/white ears med.png');
 			
 			// Front hair
-			this.game.load.image('front0', 'effects/normal hair fore.png');
+			this.game.load.image('front0', 'face elements/normal hair fore.png');
 			
 			// Back hair
-			this.game.load.image('back0', 'effects/girl hair back.png');
+			this.game.load.image('back0', 'face elements/girl hair back.png');
 			
 			// Face
-			this.game.load.image('face0', 'effects/facebase white.png');
+			this.game.load.image('face0', 'face elements/facebase white.png');
 		},
 
 		create: function () {
